7569b6492faddc765b7d20d41152036a5e3e3272
kremail <roman_kayashev@mail.ru>
2022-02-15 19:35:08 +0500

    Changed

--- script.py
+++ script.py
@@ -7,4 +7,7 @@ r = requests.get(url=URL)
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, 'html.parser')
 
-print(soup.select_one('div[class^="col-md-2 col-xs-9 _right mono-num"]').text)
+exchange_rate = soup.select_one(
+    'div[class^="col-md-2 col-xs-9 _right mono-num"]').text
+exchange_rate = exchange_rate[0:exchange_rate.find(" ")]
+print(exchange_rate)

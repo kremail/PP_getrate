99a0c2618f2b4fca73a298d332d5e18046932bee
kremail <roman_kayashev@mail.ru>
2022-02-15 21:12:57 +0500

    Some changes

--- script.py
+++ script.py
@@ -2,12 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://cbr.ru"
-data = {}
 r = requests.get(url=URL)
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, 'html.parser')
 
-exchange_rate = soup.select_one(
+exchange_rate_t = soup.select_one(
     'div[class^="col-md-2 col-xs-9 _right mono-num"]').text
-exchange_rate = exchange_rate[0:exchange_rate.find(" ")]
+exchange_rate_t = exchange_rate_t[0:exchange_rate_t.find(" ")]
+exchange_rate_t = exchange_rate_t.replace(",", ".")
+exchange_rate = float(exchange_rate_t)
 print(exchange_rate)

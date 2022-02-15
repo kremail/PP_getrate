36fbac4185dceeba817b313fc1c1000a21b6f647
kremail <romankayashev@mail.ru>
2022-02-15 23:21:43 +0500

    Code changes

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,10 +6,14 @@ URL = "https://cbr.ru"
 r = requests.get(url=URL)
 if r.status_code == 200:
     soup = BeautifulSoup(r.text, 'html.parser')
-
-exchange_rate_t = soup.select_one(
-    'div[class^="col-md-2 col-xs-9 _right mono-num"]').text
-exchange_rate_t = exchange_rate_t[0:exchange_rate_t.find(" ")]
-exchange_rate_t = exchange_rate_t.replace(",", ".")
-exchange_rate = float(exchange_rate_t)
-print(exchange_rate)
+    exchange_rate_t = soup.select_one(
+        'div[class^="col-md-2 col-xs-9 _right mono-num"]').text
+    exchange_rate_t = exchange_rate_t[0:exchange_rate_t.find(" ")]
+    exchange_rate_t = exchange_rate_t.replace(",", ".")
+    exchange_rate = float(exchange_rate_t)
+    exchange_rate_date_t = soup.select_one(
+        'div[class^="col-md-2 col-xs-7 _right"]').text
+    exchange_rate_date = datetime.strptime(exchange_rate_date_t, '%d.%m.%Y')
+    print(datetime.strftime(exchange_rate_date, '%d.%m.%y'), " ", exchange_rate)
+else:
+    print("Connection error")
